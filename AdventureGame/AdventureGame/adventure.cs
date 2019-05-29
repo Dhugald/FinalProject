a4aa268ae428871cdb7ab6ff0d550f451708b619
CodySmithZ <csmit444@gmail.com>
2019-05-29 14:30:37 +1200

    Added structs

--- AdventureGame/AdventureGame/adventure.cs
+++ AdventureGame/AdventureGame/adventure.cs
@@ -8,8 +8,26 @@ namespace AdventureGame
 {
     class adventure
     {
+        public struct Player
+        {
+            public int researchPoints;         
+        }
+        public struct Ship
+        {
+            public int health;
+            public int fuel;
+        }
+        public struct Planet
+        {
+            public string name;
+            public string environmnet;
+            public int fuelLevel;
+            public bool task;
+        }
         static void Main(string[] args)
         {
+            string[] inventory = new string[7];
+            
             Console.WriteLine("hello");
             Console.WriteLine("test");
             Console.WriteLine("test2");
