ee271a5c5d0bc23bf89a2b8a60ec21fa89b4e269
Dhugald Spry <SPRYDL1@student.op.ac.nz>
2019-05-30 09:39:40 +1200

    Added the start of the menu and the select difficulty menu

--- AdventureGame/AdventureGame/adventure.cs
+++ AdventureGame/AdventureGame/adventure.cs
@@ -69,23 +69,73 @@ namespace AdventureGame
 
         public static void GameInitialization()
         {
+
         }
 
         public static void MainMenu()
         {
-    
-        }
+            Console.WriteLine("Please select a menu option");
+            Console.WriteLine("1        Set Difficulty");
+            Console.WriteLine("2        ");
+            Console.WriteLine("3        ");
+            Console.WriteLine("_______________________________");
+            string temp = Console.ReadLine();
+            int menuOption = Convert.ToInt32(temp);
+            Console.Clear();
+            switch (menuOption)
+            {
+                case 1:
+                    SetDifficulty();
+                    break;
 
+            }
+        }
+        public static void SetDifficulty()
+        {
+                      Console.WriteLine("Please select a difficulty: ");
+            Console.WriteLine("1        Easy");
+            Console.WriteLine("2        Regular");
+            Console.WriteLine("3        Hard");
+            string temp = Console.ReadLine();
+            int Difficulty = Convert.ToInt32(temp);
+            Ship[] shipStats = new Ship[1];
+            int count = 0;
+            if (Difficulty > 3)
+            {
+                Console.WriteLine("Please enter a vaild option menu");
+                temp = Console.ReadLine();
+                Difficulty = Convert.ToInt32(temp);
+            }
+
+            switch (Difficulty)
+            {
+                case 1:
+                    shipStats[count].health = 150;
+                    shipStats[count].fuel = 150;
+                    break;
+                case 2:
+                    shipStats[count].health = 100;
+                    shipStats[count].fuel = 100;
+                    break;
+
+                case 3:
+                    shipStats[0].health = 50;
+                    shipStats[0].fuel = 50;
+                    break;
+            }
+            Console.Clear();
+
+            Console.WriteLine($"The ships starting health is: {shipStats[0].health}");
+            Console.WriteLine($"The ships starting fuel is: {shipStats[0].fuel}");
+            Console.ReadLine();
+        }
         static void Main(string[] args)
         {
             string[] inventory = new string[7];
             Planet[] planets = new Planet[8];
-            Console.WriteLine("hello");
-            Console.WriteLine("test");
-            Console.WriteLine("test2");
+            
 
-            int i = new int[10];
-            i[0] = 20
+            MainMenu();
         }
     }
 }
